83305a4e2a2bc66e2d9ba338f169deb299ed7334
Matt Ronchetto <hello@doamatto.xyz>
2021-05-28 11:14:50 -0700

    chore: created a functional example™

---
+++ example/example.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'package:yamh/yamh.dart';
+
+void main() async {
+  await yamh_str('**This** is a *markdown* string.', null).then((String str) {
+    print(str); // <p><strong>This</strong> is a <em>markdown</em> string.</p>
+  });
+
+  final file = File('../test/test.md');
+  yamh_file(file, null);
+}
